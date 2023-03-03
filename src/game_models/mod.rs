d4e98d8b4bc994ebbcaede77e2ba627545a25213
hossville <hoss.dev00@gmail.com>
2023-03-02 22:15:11 -0600

    finished get character, save character, split models out to separate file/mod

---
+++ src/game_models/mod.rs
@@ -0,0 +1,20 @@
+
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct User {
+  pub name: String,
+  pub firstname: String,
+  pub surname: String,
+  #[serde(skip_deserializing)]
+  pub id: String
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct CreateResponse {
+  pub character_id: String
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct GetCharacterRequest {
+  pub character_id: String
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
 extern crate redis;
 extern crate rocket_contrib;
 extern crate serde_json;
-//use json::JsonValue;
-use uuid::Uuid;
 
-//use serde::Deseria3ze;
+mod game_models;
+
+use crate::game_models::CreateResponse;
+use crate::game_models::GetCharacterRequest;
+use crate::game_models::User;
+
+use uuid::Uuid;
 
 #[macro_use] extern crate rocket;
 
-
-#[derive(serde::Deserialize, serde::Serialize)]
-pub struct User {
-  pub name: String,
-  pub firstname: String,
-  pub surname: String,
-  #[serde(skip_deserializing)]
-  pub id: String
-}
-
 #[post("/character", format = "application/json", data = "<input>")]
-fn create_character(input: String) {
-    let mut data : User = serde_json::from_str(&input).unwrap();
+fn create_character(input: String) -> String {
+    let mut data :  User = serde_json::from_str(&input).unwrap();
     
-    data.id = Uuid::new_v4().to_string();
+    let uuid: Uuid = Uuid::new_v4();
 
+    data.id = uuid.to_string();
     
     let mut conn = redis::Client::open("redis://localhost:6379")
     .expect ("invalid connection url")
@@ -31,42 +26,52 @@ fn create_character(input: String) {
     .expect("failed to connect to Redis");
 
     let _: () = redis::cmd("SET")
-    .arg(&data.id)
+    .arg(uuid.to_string())
     .arg(serde_json::to_string(&data).unwrap())
     .query(&mut conn)
     .expect("failed to execute SET for 'foo'");
 
     let answer: String = redis::cmd("GET")
-    .arg(data.id)
+    .arg(uuid.to_string())
     .query(&mut conn)
     .expect("failed to execute GET for for foo");
     println!("character saved {}",answer);
 
+    let response: CreateResponse = CreateResponse {
+      character_id: uuid.to_string()
+    };
+
+  return serde_json::to_string(&response).unwrap()
+    
 
 
 }
 
-#[get("/character")]
-fn get_characters() {
+#[get("/character/<character_id>")]
+fn get_character(character_id : String) -> String {
 
+  let data : GetCharacterRequest  = GetCharacterRequest {
+    character_id : format!("{}",character_id.as_str())
+  };
+    
     let mut conn = redis::Client::open("redis://localhost:6379")
     .expect ("invalid connection url")
     .get_connection()
     .expect("failed to connect to Redis");
 
-    let answer: Vec<String> = redis::cmd("KEYS")
-    .arg("*")
+    let answer: String = redis::cmd("GET")
+    .arg(data.character_id)
     .query(&mut conn)
     .expect("failed to execute GET for for foo");
 
     println!("character retrieved {:?}",answer);
 
+    return answer;
+
 }
 
-//1. create new character or load existing character
-//2. create new game or load existing game
 #[launch]
 fn my_server() -> _ {
 
-   rocket::build().mount("/", routes![create_character,get_characters])
+   rocket::build().mount("/", routes![create_character,get_character])
 }
